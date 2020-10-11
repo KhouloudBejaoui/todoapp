fbbc714681912c7c077c3c29b7ee563814ada800
khouloud711 <bejaouikhouloud98@gmail.com>
2020-10-11 12:07:08 +0200

    Updates

---
+++ precache-manifest.de38ec646467255050b93a9bec2749da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c9e2533f5be2eb4535b7f98b99b00e",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "0d7f7972db77371a20ae",
+    "url": "/todoapp/static/css/main.29c3923b.chunk.css"
+  },
+  {
+    "revision": "f864dc11f739e6dd159f",
+    "url": "/todoapp/static/js/2.858bf58f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.858bf58f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d7f7972db77371a20ae",
+    "url": "/todoapp/static/js/main.8908b9d1.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
